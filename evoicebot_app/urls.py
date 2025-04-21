614d0ef1127b27e5b61f538cdd961474c1bed3d9
ernestilchenko <7777erik777@gmail.com>
2025-04-21 15:57:10 +0200

    change urls

--- evoicebot_app/urls.py
+++ evoicebot_app/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('app/register/', register, name='register'),
     path('app/logout/', logout, name='logout'),
 
-    path('dashboard/', dashboard, name='dashboard'),
-    path('dashboard/teams/', team_list, name='team_list'),
-    path('dashboard/teams/<uuid:uuid>/', team_detail, name='team_detail'),
-    path('dashboard/teams/create/', create_team, name='create_team'),
-    path('dashboard/projects/', project_list, name='project_list'),
-    path('dashboard/projects/<int:id>/', project_detail, name='project_detail'),
-    path('dashboard/projects/create/', create_project, name='create_project'),
+    path('app/dashboard/', dashboard, name='dashboard'),
+    path('app/dashboard/teams/', team_list, name='team_list'),
+    path('app/dashboard/teams/<uuid:uuid>/', team_detail, name='team_detail'),
+    path('app/dashboard/teams/create/', create_team, name='create_team'),
+    path('app/dashboard/projects/', project_list, name='project_list'),
+    path('app/dashboard/projects/<int:id>/', project_detail, name='project_detail'),
+    path('app/dashboard/projects/create/', create_project, name='create_project'),
 ]
